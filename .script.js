96bd27b9fd29f172fa2dc942653b5b88d0982a0f
Alan Schultz <ajschultz77@gmail.com>
2021-02-10 11:50:16 -0500

    finished completing special characters and alphabets

--- .script.js
+++ .script.js
@@ -1,7 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var alphabet = []
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 function askQuestion() {
     // This function needs to collect user responses for password criteria and return it to the generate password function
